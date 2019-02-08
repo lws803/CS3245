ced655f57b2d6e179010ab2d1ff48b57a83a1245
lws803 <lws803@gmail.com>
2019-02-08 17:48:57 +0800

    Added implementation for search trees

---
+++ bst_prefix_test.py
@@ -0,0 +1,49 @@
+class Node:
+
+    def __init__(self, data):
+
+        self.left = None
+        self.right = None
+        self.data = data
+
+    def insert(self, data):
+# Compare the new value with the parent node
+        if self.data:
+            if data < self.data:
+                if self.left is None:
+                    self.left = Node(data)
+                else:
+                    self.left.insert(data)
+            elif data > self.data:
+                if self.right is None:
+                    self.right = Node(data)
+                else:
+                    self.right.insert(data)
+        else:
+            self.data = data
+
+# Print the tree
+    def PrintTree(self):
+        if self.left:
+            self.left.PrintTree()
+        print( self.data),
+        if self.right:
+            self.right.PrintTree()
+
+# Use the insert method to add nodes
+root = Node("")
+
+combined_string = ""
+for c in "human":
+    combined_string += c
+    root.insert(combined_string)
+
+combined_string = ""
+for c in "hummmus":
+    combined_string += c
+    root.insert(combined_string)
+
+
+root.PrintTree()
+
+# We can traverse the tree to find out our posting lists for the prefix we entered
