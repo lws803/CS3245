bc118c5cc797fee954f017bc16b04c881aea578f
lws803 <lws803@gmail.com>
2019-02-15 01:39:14 +0800

    updated unigram_test code

--- experiment_scripts/unigram_test.py
+++ experiment_scripts/unigram_test.py
@@ -5,8 +5,7 @@ import nltk
 text_collection_PC = ["I Don't Want To Go", "A Groovy Kind Of Love", "You Can't Hurry Love", "This Must Be Love", "Take Me With You"]
 text_collection_AS = ["All Out Of Love", "Here I Am", "I Remember Love", "Love Is All", "Don't Tell Me"]
 
-
-vocab = {}
+vocab = {} # We store vocab here to count the universal number of different vocabs available****
 text_collection_PC_freq = {}
 text_collection_AS_freq = {}
 total_text_collection_PC_freq = 0
@@ -35,53 +34,36 @@ for titles in text_collection_PC:
 
 
 print (len(vocab))
-print "Total for AS, including total vocab size: ", (total_text_collection_AS_freq + len(vocab))
-print "Total for PC, including total vocab size: ", (total_text_collection_PC_freq + len(vocab))
-print "=========================="
+print ("Total for AS, including total vocab size: ", (total_text_collection_AS_freq + len(vocab)))
+print ("Total for PC, including total vocab size: ", (total_text_collection_PC_freq + len(vocab)))
+print ("==========================")
 
-
-# Find probability of a text "I Don't Love You" in text_collection_PC
-
-
-# Add one smoothing before querying
 
 query = "I Remember You"
 
-for word in query.split():
-    vocab[word] = 1
 
-    if (word not in text_collection_PC_freq):
-        text_collection_PC_freq[word] = 1
-        total_text_collection_PC_freq += 1
+probability_AS = 0
+probability_PC = 0
 
 
 for word in query.split():
-    vocab[word] = 1
-
-    if (word not in text_collection_AS_freq):
-        text_collection_AS_freq[word] = 1
-        total_text_collection_AS_freq += 1
-
-
-
-# print "Total for PC (NEW), including total vocab size: ", (total_text_collection_PC_freq + len(vocab))
-
-probability_AS = 1.0
-probability_PC = 1.0
-
+    if (word in text_collection_PC_freq):
+        probability_PC += math.log(text_collection_PC_freq[word]/float(total_text_collection_PC_freq + len(vocab)))
 
 for word in query.split():
-    probability_PC *= text_collection_PC_freq[word]/float(total_text_collection_PC_freq + len(vocab))
+    if (word in text_collection_AS_freq):
+        probability_AS += math.log(text_collection_AS_freq[word]/float(total_text_collection_AS_freq + len(vocab)))
+        
+        
+print ("Air Supply", probability_AS)
+print ("Phil Collins", probability_PC)
 
-for word in query.split():
-    probability_AS *= text_collection_AS_freq[word]/float(total_text_collection_AS_freq + len(vocab))
+
+# ## Final result
 
 
 if (probability_PC > probability_AS):
-    print "This title is from Air Supply"
+    print ("This title is from Air Supply")
 else:
-    print "This title is from Phil Collins"
-
-
-
+    print ("This title is from Phil Collins")
 
