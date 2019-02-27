6bef43d967c8e2aff48aa82b522288162196f55d
lws803 <lws803@gmail.com>
2019-02-27 23:06:03 +0800

    Added skip list and full generated postings + dictionary list

--- HW2/index.py
+++ HW2/index.py
@@ -5,11 +5,14 @@ import nltk
 import os
 import sys
 import getopt
+import math
 
 from nltk.stem.porter import PorterStemmer
 
+FIXED_WIDTH = 4
+
 def encoder(decimal):
-    return hex(decimal)[2:].zfill(4)
+    return hex(decimal)[2:].zfill(FIXED_WIDTH)
 
 stemmer = PorterStemmer()
 
@@ -61,7 +64,7 @@ for filename in os.listdir(input_directory):
                 else:
                     index[word] = {int(filename)}
     count += 1
-    if (count >= 10): break
+    # if (count >= 10): break
 
 
 for key in index.keys():
@@ -71,18 +74,29 @@ sorted_keys = sorted(index.keys())
 
 for key in sorted_keys:
     # Print out to dictionary.txt and postings.txt
-    dictionary_data.write(key + '\n')
-    
+    starting_cursor = postings_data.tell()
+    # Adds the key_term, size, starting cursor
+    dictionary_data.write(key + ' ' + str(len(index[key])) + ' ' + str(starting_cursor) + '\n')
+
+    skip_spaces = math.sqrt(len(index[key]))
+    skip_spaces = int(skip_spaces)
+
     position = -1
     for doc_id in index[key]:
         position += 1
         hex_val = encoder(doc_id)
-
+        
         # TODO: Need a function that gives the value of the skip pointer (i.e. how many bytes to skip)
         if position == len(index[key]) - 1:
+            # TODO: Recalculate again if we need these many hexes (technically only need 4)
             postings_data.write(str(hex_val) + "ffff")
         else:
-            postings_data.write(str(hex_val) + "0000")
+            postings_data.write(str(hex_val))
+            # TODO: Run through this combinational logic again and make sure its right
+            if (skip_spaces >= 2 and not(position%skip_spaces) and position+skip_spaces < len(index[key])):
+                postings_data.write(str(encoder(position+skip_spaces)))
+            else:
+                postings_data.write("0000")
 
 dictionary_data.close()
 postings_data.close()
