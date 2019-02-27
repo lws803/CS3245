9c7e90e68004f5106b49a7bd0c24fc59be1209e3
aaryamNUS <aaryamps1@gmail.com>
2019-02-27 19:32:39 +0800

    Hexadecimal postings

--- HW2/index.py
+++ HW2/index.py
@@ -6,7 +6,7 @@ import os
 import sys
 import getopt
 
-from nltk.stem.porter import *
+from nltk.stem.porter import PorterStemmer
 
 def encoder(decimal):
     return hex(decimal)[2:].zfill(4)
@@ -72,11 +72,17 @@ sorted_keys = sorted(index.keys())
 for key in sorted_keys:
     # Print out to dictionary.txt and postings.txt
     dictionary_data.write(key + '\n')
-
+    
+    position = -1
     for doc_id in index[key]:
+        position += 1
         hex_val = encoder(doc_id)
-        postings_data.write(str(hex_val) + ",")
-    postings_data.write('\n')
+
+        # TODO: Need a function that gives the value of the skip pointer (i.e. how many bytes to skip)
+        if position == len(index[key]) - 1:
+            postings_data.write(str(hex_val) + "ffff")
+        else:
+            postings_data.write(str(hex_val) + "0000")
 
 dictionary_data.close()
 postings_data.close()
