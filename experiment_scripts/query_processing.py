16d05052e6472fa12861a022507f284cc6cf8bce
lws803 <lws803@gmail.com>
2019-02-28 13:25:29 +0800

    Fixed shunting yard algo

--- experiment_scripts/query_processing.py
+++ experiment_scripts/query_processing.py
@@ -2,16 +2,18 @@ import nltk
 
 query = "test AND set"
 query2 = "test AND NOT set"
-query3 = "test AND (set OR cool)"
-query4 = "bill OR Gates AND (vista OR XP) AND NOT mac"
+query3 = "Matthew AND NOT (Wilson AND Aaryam)"
+query4 = "bill AND Gates AND Aaryam AND NOT Wilson OR Matthew AND James AND tg"
 
 stack = []
 queue = []
-operators = {"NOT": 3, "AND": 4, "OR": 5}
+operators = {"NOT": 5, "AND": 4, "OR": 3}
 
+# TODO: change this for loop to iterate by index instead
+# TODO: Find a way to deal with AND NOT/ OR NOT
 
 # Shunting yard algorithm
-for token in nltk.word_tokenize(query4):
+for token in nltk.word_tokenize(query3):
     # print token
     if (token == "("):
         stack.append(token)
@@ -21,8 +23,8 @@ for token in nltk.word_tokenize(query4):
         stack.pop()
 
     elif (token in operators):
-        if (len(stack) != 0 and stack[-1] != "(" and operators[stack[-1]] < operators[token]):
-            queue.append(stack.pop())
+        if (len(stack) != 0 and stack[-1] != "(" and operators[stack[-1]] <= operators[token]):
+            # queue.append(stack.pop())
             queue.append(token)
         else:
             stack.append(token)
@@ -36,4 +38,4 @@ while (len(stack) > 0):
 
 # Create a postfix processor for this
 for item in queue:
-    print item
+    print (item)
